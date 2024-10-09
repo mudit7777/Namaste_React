a0e51c340c28cb4a9b1b46a8b86b6a8c2da033af
ARORA Mudit <mudit.arora@soprasteria.com>
2024-10-09 10:25:46 +0530

    power of render

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World from React"
+  "Billionaire Mudit💥"
 );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "headingofChild" }, "I'm a h1 tag")
-  )
-);
-
-console.log(parent);
-// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent);
+
+root.render(heading);
