a64223544bd53895f236ef447d24db4b1980130d
ARORA Mudit <mudit.arora@soprasteria.com>
2024-10-17 13:07:48 +0530

    play with components and elements

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "heading" }, "Mudit💥");
+const Title = () => (
+  <div>
+    <h1 className="titleComponent">this is Title Component</h1>
+  </div>
+);
+const element = <h1> {heading} Element in swwweact </h1>;
+const ComponentOne = () => (
+  <div>
+    {element}
+    <Title /> {element}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<ComponentOne />);
